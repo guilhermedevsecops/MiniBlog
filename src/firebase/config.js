893b7aeacb2e550332f1c615bf5d6a7401e5714a
Guilherme Henrique de Sousa Jesus <160426053+guilhermedevsecops@users.noreply.github.com>
2024-05-22 20:36:07 -0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBFTCZVO1O__u4uMN2V-kluvUPhdleJ1-g",
-  authDomain: "miniblog-ref-5aef4.firebaseapp.com",
-  projectId: "miniblog-ref-5aef4",
-  storageBucket: "miniblog-ref-5aef4.appspot.com",
-  messagingSenderId: "884077777860",
-  appId: "1:884077777860:web:19e631fcf96449798d0a91"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
